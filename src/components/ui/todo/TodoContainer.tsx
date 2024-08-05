e22e8e67fd8efe2ed987bede4bdf407fc7e7784c
ismail <coderismail22@gmail.com>
2024-08-05 11:29:44 +0600

    add message if there are no pending task

--- src/components/ui/todo/TodoContainer.tsx
+++ src/components/ui/todo/TodoContainer.tsx
@@ -13,14 +13,22 @@ const TodoContainer = () => {
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl p-[3px] flex flex-col gap-5">
         <div className="bg-white p-5 w-full h-full rounded-lg">
-          {todos.map((todo) => (
-            <TodoCard id={todo.id} title={todo.title} description={todo.description} />
-          ))}
+          {todos.length > 0 ? (
+            <div>
+              {todos.map((todo) => (
+                <TodoCard
+                  id={todo.id}
+                  title={todo.title}
+                  description={todo.description}
+                />
+              ))}
+            </div>
+          ) : (
+            <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center rounded-lg">
+              <p>There is no task pending !</p>
+            </div>
+          )}
         </div>
-        {/* If there are no todos */}
-        {/* <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center rounded-lg">
-          <p>There is no task pending !</p>
-        </div> */}
       </div>
     </div>
   );
